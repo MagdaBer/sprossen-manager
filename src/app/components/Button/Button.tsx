dfc49dc643c936101df6188526aad5fa319fbb36
MagdaBer <magda.berger@gmx.de>
2021-09-10 20:39:57 +0200

    Create Button function

---
+++ src/app/components/Button/Button.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import type { ReactNode } from 'react';
+import styles from './Button.module.css';
+import Typography from '../Typography/Typography';
+
+type ButtonProps = {
+  children: ReactNode;
+};
+
+function Button({ children }: ButtonProps): JSX.Element {
+  return (
+    <button className={styles.button}>
+      <Typography size="sAction">{children}</Typography>
+    </button>
+  );
+}
+
+export default Button;

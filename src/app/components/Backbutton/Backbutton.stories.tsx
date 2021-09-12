be89681fa31a7ae5323ee94130b728f8478023a1
MagdaBer <magda.berger@gmx.de>
2021-09-12 17:44:35 +0200

    Write backButton story

---
+++ src/app/components/Backbutton/Backbutton.stories.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Backbutton from './Backbutton';
+
+export default {
+  title: 'Component/Backbutton',
+  component: Backbutton,
+};
+export const backButton = (): JSX.Element => <Backbutton />;

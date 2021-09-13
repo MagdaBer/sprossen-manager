b5bb34999675b64507dde53030741bc50aba3a7f
MagdaBer <magda.berger@gmx.de>
2021-09-13 21:35:00 +0200

    Create IconType type and move iconSizeMap

--- src/app/components/Icons/Icons.tsx
+++ src/app/components/Icons/Icons.tsx
@@ -2,18 +2,23 @@ import React from 'react';
 import DropIcon from '../assets/Drop';
 import LeafIcon from '../assets/Leaf';
 
-export type IconProps = {
-  iconType: 'dropBig' | 'dropSmall' | 'leafBig' | 'leafSmall';
+type IconType = 'dropBig' | 'dropSmall' | 'leafBig' | 'leafSmall';
+
+type IconProps = {
+  iconType: IconType;
+};
+
+const iconSizeMap: {
+  [iconType in IconType]: JSX.Element;
+} = {
+  dropBig: <DropIcon width="1.1813rem" height="2.438rem" />,
+  dropSmall: <DropIcon width="1rem" height="0.938rem" />,
+  leafBig: <LeafIcon width="2.492rem" height="1.813rem" />,
+  leafSmall: <LeafIcon width="1.115rem" height="0.773rem" />,
 };
 
 const Icons = ({ iconType }: IconProps): JSX.Element => {
-  const iconSizeMap = {
-    dropBig: <DropIcon width="1.1813rem" height="2.438rem" />,
-    dropSmall: <DropIcon width="1rem" height="0.938rem" />,
-    leafBig: <LeafIcon width="2.492rem" height="1.813rem" />,
-    leafSmall: <LeafIcon width="1.115rem" height="0.773rem" />,
-  };
-
   return <div>{iconSizeMap[iconType]}</div>;
 };
+
 export default Icons;

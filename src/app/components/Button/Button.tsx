d80cbf5a9534b78d36839e7513ea21f0bf08a935
MagdaBer <magda.berger@gmx.de>
2021-09-13 16:25:35 +0200

    Add onClick to Button function

--- src/app/components/Button/Button.tsx
+++ src/app/components/Button/Button.tsx
@@ -6,11 +6,16 @@ import Typography from '../Typography/Typography';
 type ButtonProps = {
   children: ReactNode;
   className?: string;
+  onButtonClick: () => void;
 };
 
-function Button({ children, className }: ButtonProps): JSX.Element {
+function Button({
+  children,
+  className,
+  onButtonClick,
+}: ButtonProps): JSX.Element {
   return (
-    <button className={`${styles.button} ${className}`}>
+    <button className={`${styles.button} ${className}`} onClick={onButtonClick}>
       <Typography size="xsAction">{children}</Typography>
     </button>
   );

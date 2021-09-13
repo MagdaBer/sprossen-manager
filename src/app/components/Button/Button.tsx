7b71f134f26f0bbce070a9c6f489937c7a42bf21
MagdaBer <magda.berger@gmx.de>
2021-09-13 12:45:30 +0200

    Add className prop to button

--- src/app/components/Button/Button.tsx
+++ src/app/components/Button/Button.tsx
@@ -5,11 +5,12 @@ import Typography from '../Typography/Typography';
 
 type ButtonProps = {
   children: ReactNode;
+  className?: string;
 };
 
-function Button({ children }: ButtonProps): JSX.Element {
+function Button({ children, className }: ButtonProps): JSX.Element {
   return (
-    <button className={styles.button}>
+    <button className={`${styles.button} ${className}`}>
       <Typography size="xsAction">{children}</Typography>
     </button>
   );

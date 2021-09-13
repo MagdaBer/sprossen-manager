42583c8fb3e3989e2323b29ea1e1bf2486a1641c
MagdaBer <magda.berger@gmx.de>
2021-09-13 12:13:27 +0200

    Change sAction to xsAction

--- src/app/components/Typography/Typography.tsx
+++ src/app/components/Typography/Typography.tsx
@@ -3,14 +3,14 @@ import type { ReactNode } from 'react';
 import styles from './Typography.module.css';
 
 type TypographyProps = {
-  size: 'xs' | 's' | 'm' | 'sAction' | 'l' | 'xl';
+  size: 'xs' | 's' | 'm' | 'xsAction' | 'l' | 'xl';
   children: ReactNode;
 };
 
 const sizeMap = {
   xs: `${styles.textExtraSmall} ${styles.medium}`,
+  xsAction: `${styles.textExtraSmall} ${styles.action}`,
   s: `${styles.textSmall} ${styles.medium}`,
-  sAction: `${styles.textSmall} ${styles.action}`,
   m: `${styles.textMedium} ${styles.medium}`,
   l: `${styles.textLarge} ${styles.medium}`,
   xl: `${styles.textExtraLarge} ${styles.regular}`,
@@ -23,7 +23,7 @@ export default function Typography({
   switch (size) {
     case 'xs':
     case 's':
-    case 'sAction':
+    case 'xsAction':
       return <p className={sizeMap[size]}>{children}</p>;
     case 'm':
       return <h3 className={sizeMap[size]}>{children}</h3>;

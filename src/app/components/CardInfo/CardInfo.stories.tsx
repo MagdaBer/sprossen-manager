044bf33c2c09488ccfa6f19e4b2377422fc4adcd
MagdaBer <magda.berger@gmx.de>
2021-09-16 15:07:43 +0200

    Write basic CardInfo story

---
+++ src/app/components/CardInfo/CardInfo.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import CardInfo from './CardInfo';
+
+export default {
+  title: 'Component/CardInfo',
+  component: CardInfo,
+};
+
+export const Card = (): JSX.Element => <CardInfo />;

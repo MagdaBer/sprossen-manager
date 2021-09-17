e22d1ffff277294ebebcadf1e2afa9c3e33584c3
MagdaBer <magda.berger@gmx.de>
2021-09-17 10:26:54 +0200

    Add all required components to CardInfo

--- src/app/components/CardInfo/CardInfo.tsx
+++ src/app/components/CardInfo/CardInfo.tsx
@@ -1,10 +1,52 @@
 import React from 'react';
+import Button from '../Button/Button';
+import Icons from '../Icons/Icons';
+import InfoLink from '../InfoLink/InfoLink';
+import Typography from '../Typography/Typography';
 import style from './CardInfo.module.css';
 
-export const CardInfo = (): JSX.Element => {
+export type CardInfoProps = {
+  image: string;
+  header: string;
+  hours: string;
+  days: string;
+  onClickAdd: () => void;
+};
+
+export const CardInfo = ({
+  image,
+  header,
+  hours,
+  days,
+  onClickAdd,
+}: CardInfoProps): JSX.Element => {
   return (
     <article className={style.card}>
       <div className={style.circle} />
+      <img className={style.image} src={image} />
+      <section className={style.cardContent}>
+        <p>
+          <Typography size="m" className={style.header}>
+            {header}
+          </Typography>
+        </p>
+        <div className={style.soak}>
+          <Icons iconType="dropSmall" />
+          <p>
+            <Typography size="s">Einweichen: {hours}</Typography>
+          </p>
+        </div>
+        <div className={style.germinate}>
+          <Icons iconType="leafSmall" />
+          <p>
+            <Typography size="s">Keimen: {days}</Typography>
+          </p>
+        </div>
+      </section>
+      <section className={style.cardButtons}>
+        <Button onClick={onClickAdd} children="Hinzufügen" />
+        <InfoLink children="Info" />
+      </section>
     </article>
   );
 };

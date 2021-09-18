0d6fb7208c7e2226203dee5520902aaa847e4cd0
MagdaBer <magda.berger@gmx.de>
2021-09-18 19:20:57 +0200

    Add two types of header to see the difference

--- src/app/components/CardInfo/CardInfo.stories.tsx
+++ src/app/components/CardInfo/CardInfo.stories.tsx
@@ -19,7 +19,7 @@ export const CardBig = (): JSX.Element => (
   <CardInfo
     type="big"
     image="src/app/components/assets/Buckwheat.png"
-    header="Sonnenblumen kerne"
+    header="Buchweizen"
     hours="8 Stunden"
     days="6 Tage"
     textfield="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."

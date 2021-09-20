2cdd786eae05de6376b6c46a0dce0a10d86869bb
MagdaBer <magda.berger@gmx.de>
2021-09-20 11:36:29 +0200

    Extend story with required props

--- src/app/components/CardStatus/CardStatus.stories.tsx
+++ src/app/components/CardStatus/CardStatus.stories.tsx
@@ -7,5 +7,17 @@ export default {
 };
 
 export const Card = (): JSX.Element => (
-  <CardStatus image="src/app/components/assets/Buckwheat.png" />
+  <CardStatus
+    image="src/app/components/assets/Buckwheat.png"
+    header="Buchweizen"
+    days="8 Tage"
+    hours="8 Stunden"
+    status="Keimen"
+    startdate="03.09."
+    starttime="20:00 Uhr"
+    enddate="11.09."
+    endtime="20:00 Uhr"
+    onClickChangeStatus={() => console.log('changed')}
+    onClickRemove={() => console.log('removed')}
+  />
 );

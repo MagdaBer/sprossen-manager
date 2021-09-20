14a4d0e6f10afd536e9681f0c169ac3ced5c1d82
MagdaBer <magda.berger@gmx.de>
2021-09-20 11:35:18 +0200

    Create content elements for CardStatus function

--- src/app/components/CardStatus/CardStatus.tsx
+++ src/app/components/CardStatus/CardStatus.tsx
@@ -1,14 +1,67 @@
 import React from 'react';
+import Button from '../Button/Button';
+import Icons from '../Icons/Icons';
+import Typography from '../Typography/Typography';
 import style from './CardStatus.module.css';
 
 type CardStatusProps = {
   image: string;
+  header: string;
+  status?: 'Einweichen' | 'Keimen';
+  hours: string;
+  days: string;
+  startdate: string;
+  starttime: string;
+  enddate: string;
+  endtime: string;
+  onClickChangeStatus: () => void;
+  onClickRemove: () => void;
 };
-export const CardStatus = ({ image }: CardStatusProps): JSX.Element => {
+export const CardStatus = ({
+  image,
+  header,
+  status = 'Einweichen',
+  hours,
+  days,
+  startdate,
+  starttime,
+  enddate,
+  endtime,
+  onClickChangeStatus,
+  onClickRemove,
+}: CardStatusProps): JSX.Element => {
   return (
     <article className={style.card}>
       <div className={style.circle} />
       <img className={style.image} src={image} />
+      <section className={style.cardContent}>
+        <Typography size="m" className={style.header} children={header} />
+        <Typography size="s" className={style.text} children="Status:" />
+        <Typography size="s" className={style.status} children={status} />
+        <Typography size="s" className={style.duration} children="Dauer:" />
+        <Typography
+          size="s"
+          className={style.text}
+          children={`${status === 'Einweichen' ? hours : days}`}
+        />
+        <Typography size="s" className={style.text} children="Gestartet:" />
+        <Typography
+          size="s"
+          className={style.time}
+          children={`${startdate} um ${starttime}`}
+        />
+        <Typography size="s" className={style.text} children="Fertig:" />
+        <Typography
+          size="s"
+          className={style.time}
+          children={`${enddate} um ${endtime}`}
+        />
+        <Icons
+          iconType={`${status === 'Einweichen' ? 'dropBig' : 'leafBig'}`}
+        />
+        <Button onClick={onClickChangeStatus} children="Status ändern" />
+        <Button onClick={onClickRemove} children="Entfernen" />
+      </section>
     </article>
   );
 };

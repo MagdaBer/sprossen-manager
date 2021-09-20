69d569366fed478218e4e37653d33434a49fe615
MagdaBer <magda.berger@gmx.de>
2021-09-20 16:13:43 +0200

    Add second story

--- src/app/components/CardStatus/CardStatus.stories.tsx
+++ src/app/components/CardStatus/CardStatus.stories.tsx
@@ -6,7 +6,22 @@ export default {
   component: CardStatus,
 };
 
-export const Card = (): JSX.Element => (
+export const CardSoak = (): JSX.Element => (
+  <CardStatus
+    image="src/app/components/assets/Buckwheat.png"
+    header="Sonenblumenkerne"
+    days="8 Tage"
+    hours="8 Stunden"
+    status="Einweichen"
+    startdate="03.09."
+    starttime="20:00 Uhr"
+    enddate="11.09."
+    endtime="20:00 Uhr"
+    onClickChangeStatus={() => console.log('changed')}
+    onClickRemove={() => console.log('removed')}
+  />
+);
+export const CardGerminate = (): JSX.Element => (
   <CardStatus
     image="src/app/components/assets/Buckwheat.png"
     header="Buchweizen"

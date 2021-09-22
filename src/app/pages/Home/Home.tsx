850581458303f337d9727bd4c57c74c2a24cc54c
MagdaBer <magda.berger@gmx.de>
2021-09-22 15:20:11 +0200

    Add Link to menu page

--- src/app/pages/Home/Home.tsx
+++ src/app/pages/Home/Home.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import Typography from '../../components/Typography/Typography';
 import style from './Home.module.css';
+import { Link } from 'react-router-dom';
 
 export default function Home(): JSX.Element {
   return (
     <main className={style.container}>
-      <p>
+      <Link to="/menu" className={style.header}>
         <Typography size="xl" children="Sprossen Manager" />
-      </p>
+      </Link>
     </main>
   );
 }

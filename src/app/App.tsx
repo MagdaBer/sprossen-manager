2f7ec3efa3a8213d739b875b247912d0c8f6e66a
MagdaBer <magda.berger@gmx.de>
2021-09-23 13:42:39 +0200

    Add menu route to App

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home/Home';
+import Menu from './pages/Menu/Menu';
 
 function App(): JSX.Element {
   return (
@@ -8,6 +9,9 @@ function App(): JSX.Element {
       <Switch>
         <Route path="/home">
           <Home />
+        </Route>
+        <Route path="/menu">
+          <Menu />
         </Route>
       </Switch>
     </BrowserRouter>

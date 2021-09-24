01fd4d968bfd5028a51ca3dc4b0e095074c298ad
MagdaBer <magda.berger@gmx.de>
2021-09-24 15:48:39 +0200

    Change Link from menu to landing

--- src/app/pages/Home/Home.tsx
+++ src/app/pages/Home/Home.tsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 
 export default function Home(): JSX.Element {
   return (
-    <Link to="/menu" className={style.link}>
+    <Link to="/landing" className={style.link}>
       <main className={style.container}>
         <Typography size="xl" children="Sprossen Manager" />
       </main>

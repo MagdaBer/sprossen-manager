a03433d8e2d858ed22818b4d37464db51df416ec
MagdaBer <magda.berger@gmx.de>
2021-09-24 15:42:17 +0200

    Create lib to store sprout data

---
+++ src/app/lib/sprouts.ts
@@ -0,0 +1,16 @@
+export const SPROUTS = [
+  {
+    header: 'Adzukibohne',
+    hours: '8-12 Std.',
+    days: '3-5 Tage',
+    textfield: 'blabla',
+    image: 'src/app/components/assets/Buckwheat.png',
+  },
+  {
+    header: 'Alfalfa',
+    hours: '8-12 Std.',
+    days: '7-12 Tage',
+    textfield: 'bla bla',
+    image: 'src/app/components/assets/Buckwheat.png',
+  },
+];

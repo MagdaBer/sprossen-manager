b526173538ae20c249c0061fd113509f675d4df0
MagdaBer <magda.berger@gmx.de>
2021-09-24 15:56:23 +0200

    Create path to finder page

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,16 +2,20 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Landing from './pages/Landing/Landing';
+import Finder from './pages/Finder/Finder';
 
 function App(): JSX.Element {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/">
-          <Home />
+        <Route path="/finder">
+          <Finder />
         </Route>
         <Route path="/landing">
           <Landing />
+        </Route>
+        <Route path="/">
+          <Home />
         </Route>
       </Switch>
     </BrowserRouter>

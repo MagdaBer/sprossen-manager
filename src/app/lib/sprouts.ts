eda806380b1931e032645e52e1f73f12ca6726e6
MagdaBer <magda.berger@gmx.de>
2021-09-25 08:07:14 +0200

    Add more sprout samples to style finder page

--- src/app/lib/sprouts.ts
+++ src/app/lib/sprouts.ts
@@ -1,15 +1,29 @@
 export const SPROUTS = [
   {
     header: 'Adzukibohne',
-    hours: '8-12 Std.',
-    days: '3-5 Tage',
+    hours: '8 Stunden',
+    days: '4 Tage',
     textfield: 'blabla',
     image: 'src/app/components/assets/Buckwheat.png',
   },
   {
     header: 'Alfalfa',
-    hours: '8-12 Std.',
-    days: '7-12 Tage',
+    hours: '8 Stunden',
+    days: '8 Tage',
+    textfield: 'bla bla',
+    image: 'src/app/components/assets/Buckwheat.png',
+  },
+  {
+    header: 'Alfalfa',
+    hours: '8 Stunden',
+    days: '8 Tage',
+    textfield: 'bla bla',
+    image: 'src/app/components/assets/Buckwheat.png',
+  },
+  {
+    header: 'Alfalfa',
+    hours: '8 Stunden',
+    days: '8 Tage',
     textfield: 'bla bla',
     image: 'src/app/components/assets/Buckwheat.png',
   },

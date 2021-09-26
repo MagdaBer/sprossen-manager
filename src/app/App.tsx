26ceb2ad4b7c1a6d637d4a9998e89be28c74014c
MagdaBer <magda.berger@gmx.de>
2021-09-26 09:19:56 +0200

    Add route in App to info page

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,16 +2,21 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Landing from './pages/Landing/Landing';
+import Info from './pages/Info/Info';
+import Finder from './pages/Finder/Finder';
 
 function App(): JSX.Element {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/">
-          <Home />
+        <Route path="/info/:id">
+          <Info />
         </Route>
         <Route path="/landing">
           <Landing />
+        </Route>
+        <Route path="/">
+          <Home />
         </Route>
       </Switch>
     </BrowserRouter>

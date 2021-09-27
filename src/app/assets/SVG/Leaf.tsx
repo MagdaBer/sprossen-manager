c75b18892b5ff886b83130f77ed3b15e5692ebfa
MagdaBer <magda.berger@gmx.de>
2021-09-27 18:41:54 +0200

    Fix Icons

--- src/app/assets/SVG/Leaf.tsx
+++ src/app/assets/SVG/Leaf.tsx
@@ -13,37 +13,37 @@ export default function LeafIcon(
       <path
         d="M1 10.6667C1 15.7943 3.10055 20.7118 6.83956 24.3375C10.5786 27.9632 15.6497 30.0001 20.9375 30.0001C26.2252 30.0001 31.2964 27.9632 35.0354 24.3375C38.7744 20.7118 40.875 15.7943 40.875 10.6667"
         stroke="#378657"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M20.9375 30.0001C20.9375 24.8726 23.0381 19.9551 26.7771 16.3293C30.5161 12.7036 35.5872 10.6667 40.875 10.6667"
         stroke="#378657"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M1 10.6667C3.61823 10.6667 6.21082 11.1668 8.62975 12.1384C11.0487 13.11 13.2466 14.5341 15.0979 16.3293C16.9493 18.1246 18.4179 20.2559 19.4198 22.6015C20.4218 24.9472 20.9375 27.4612 20.9375 30.0001"
         stroke="#378657"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M20.9375 1C22.9468 2.85997 24.5192 5.11784 25.5479 7.61996C26.5766 10.1221 27.0373 12.8097 26.8988 15.5"
         stroke="#378657"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M14.9762 15.5C14.8377 12.8097 15.2984 10.1221 16.3271 7.61996C17.3558 5.11784 18.9283 2.85997 20.9375 1"
         stroke="#378657"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );

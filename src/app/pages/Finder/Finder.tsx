6ba057c52efe3e2a90793acb304e62235d36db5c
MagdaBer <magda.berger@gmx.de>
2021-09-28 16:09:45 +0200

    Change backgroundimage source

--- src/app/pages/Finder/Finder.tsx
+++ src/app/pages/Finder/Finder.tsx
@@ -3,6 +3,7 @@ import Header from '../../components/Header/Header';
 import CardInfo from '../../components/CardInfo/CardInfo';
 import { SPROUTS } from '../../lib/sprouts';
 import style from './Finder.module.css';
+import page from '../../assets/images/Page.png';
 
 export default function Finder(): JSX.Element {
   function handleAddClick() {
@@ -10,7 +11,10 @@ export default function Finder(): JSX.Element {
   }
 
   return (
-    <main className={style.container}>
+    <main
+      className={style.container}
+      style={{ backgroundImage: `url(${page})` }}
+    >
       <Header children="Sprossenfinder" onClick={() => history.back()} />
       <section className={style.cards}>
         {SPROUTS.map((sprout) => (

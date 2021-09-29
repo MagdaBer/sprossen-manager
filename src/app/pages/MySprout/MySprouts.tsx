4251ae7d38d5b2f2ad2643faaeced1f7e36f254b
MagdaBer <magda.berger@gmx.de>
2021-09-29 19:17:40 +0200

    Use local storage to get clicked sprouts

--- src/app/pages/MySprout/MySprouts.tsx
+++ src/app/pages/MySprout/MySprouts.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import CardInfo from '../../components/CardInfo/CardInfo';
 import Header from '../../components/Header/Header';
 import style from './MySprouts.module.css';
+import useSproutCards from '../../hooks/useSproutCards';
 
 export default function MySprouts(): JSX.Element {
+  const { sprouts } = useSproutCards();
+
   return (
     <main className={style.container}>
       <Header children="Meine Sprossen" onClick={() => history.back()} />

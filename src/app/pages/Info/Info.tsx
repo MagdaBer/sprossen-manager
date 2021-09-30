b6d0f5cee0139f999cc08f618661e443631e813b
MagdaBer <magda.berger@gmx.de>
2021-09-30 16:12:42 +0200

    Use local storage to store sprout

--- src/app/pages/Info/Info.tsx
+++ src/app/pages/Info/Info.tsx
@@ -4,26 +4,31 @@ import Header from '../../components/Header/Header';
 import { SPROUTS } from '../../lib/sprouts';
 import { useParams } from 'react-router';
 import style from './Info.module.css';
+import useSproutCards from '../../hooks/useSproutCards';
+import type { Sprout } from '../../../types';
 
 export default function Info(): JSX.Element {
+  const { addCard } = useSproutCards();
+  function handleAddClick(sprout: Sprout) {
+    addCard(sprout);
+  }
   const { id }: { id: string } = useParams();
 
   const filteredSprout = SPROUTS.filter((sprout) => sprout.id === Number(id));
 
-  function handleOnClick() {
-    console.log('Add');
-  }
   return (
     <main className={style.container}>
       <Header children="Info" onClick={() => history.back()} />
-      {filteredSprout.map((sprout) => (
-        <CardInfo
-          key={sprout.id}
-          type="big"
-          {...sprout}
-          onClick={() => handleOnClick()}
-        />
-      ))}
+      <section className={style.card}>
+        {filteredSprout.map((sprout) => (
+          <CardInfo
+            key={sprout.id}
+            type="big"
+            {...sprout}
+            onClick={() => handleAddClick(sprout)}
+          />
+        ))}
+      </section>
     </main>
   );
 }

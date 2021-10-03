0299137e1cee275d1fc8f3007066029475072f2f
MagdaBer <magda.berger@gmx.de>
2021-10-03 21:50:48 +0200

    Change Sprout types

--- src/types.ts
+++ src/types.ts
@@ -6,12 +6,14 @@ export type Sprout = {
   id: number;
   image: string;
   header: string;
-  hours: string;
-  days: string;
+  hours: number;
+  days: number;
   textfield: string;
   ingredients: string;
   note?: string;
   status?: 'start' | 'soak' | 'germinate';
   startdate?: string;
   starttime?: string;
+  enddate?: string;
+  endtime?: string;
 };
